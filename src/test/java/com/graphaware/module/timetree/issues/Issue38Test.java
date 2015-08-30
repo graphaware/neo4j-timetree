58d68c7ba576acf0c9b33308050400c4cf8ef976
Michal Bachman <michal@graphaware.com>
2015-08-30 10:38:56 +0800

    extra test for issue #41

--- src/test/java/com/graphaware/module/timetree/issues/Issue38Test.java
+++ src/test/java/com/graphaware/module/timetree/issues/Issue38Test.java
@@ -1,8 +1,12 @@
 package com.graphaware.module.timetree.issues;
 
 import com.graphaware.test.integration.NeoServerIntegrationTest;
+import com.graphaware.test.unit.GraphUnit;
 import org.apache.http.HttpStatus;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class Issue38Test extends NeoServerIntegrationTest {
 
@@ -51,5 +55,33 @@ public class Issue38Test extends NeoServerIntegrationTest {
                 "(item)-[:Modified]->(second2)";
 
         httpClient.post(baseUrl() + "/graphaware/resttest/assertSameGraph", "{\"cypher\":\"" + cypher + "\"}", HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testNonUTCEventsAreReturned() {
+        // 24.08.2015 11:43UTC
+        // hour node will be 22 because configuration is GMT+11
+        // node Id will be 0
+        httpClient.executeCypher(baseUrl(), "CREATE (n:Item {created: 1440416586000})");
+        String response = httpClient.get(baseUrl() + "/graphaware/timetree/range/1440416586000/1440416600000/events?timezone=GMT+11", null, HttpStatus.SC_OK);
+        assertEquals("[{\"node\":{\"id\":0,\"properties\":{\"created\":1440416586000},\"labels\":[\"Item\"]},\"relationshipType\":\"Created\",\"direction\":\"INCOMING\"}]", response);
+        // add a node modification property
+        // modification time is 24.08.2015 12:02UTC
+        httpClient.executeCypher(baseUrl(), "MATCH (n) WHERE id(n) = 0 SET n.modified = 1440417779000");
+        String modificationResponse = httpClient.get(baseUrl() + "/graphaware/timetree/range/1440417779000/1440417779000/events?timezone=GMT+11", null, HttpStatus.SC_OK);
+        assertEquals("[{\"node\":{\"id\":0,\"properties\":{\"created\":1440416586000,\"modified\":1440417779000},\"labels\":[\"Item\"]},\"relationshipType\":\"Created\",\"direction\":\"INCOMING\"},{\"node\":{\"id\":0,\"properties\":{\"created\":1440416586000,\"modified\":1440417779000},\"labels\":[\"Item\"]},\"relationshipType\":\"Modified\",\"direction\":\"INCOMING\"}]", modificationResponse);
+
+        // The below query shouldn't return anything as the default timezone is UTC and the previous events were inserted with GMT+11
+        String responseFromRequestWithoutTZ = httpClient.get(baseUrl() + "/graphaware/timetree/range/1440417779000/1440417779000/events?resolution=Second", null, HttpStatus.SC_OK);
+        assertEquals("[]", responseFromRequestWithoutTZ);
+    }
+
+    @Test
+    public void brettoTest() {
+        httpClient.executeCypher(baseUrl(), "CREATE (n:Item {created: 1439881228448})");
+        httpClient.executeCypher(baseUrl(), "MATCH (n:Item) SET n.modified=1439882928756");
+        System.out.println(httpClient.executeCypher(baseUrl(), "MATCH n RETURN n, labels(n)"));
+        String response = httpClient.get(baseUrl() + "/graphaware/timetree/range/1439883028756/1439884091330/events?resolution=Second", null, HttpStatus.SC_OK);
+        assertEquals("[]", response);
     }
 }
